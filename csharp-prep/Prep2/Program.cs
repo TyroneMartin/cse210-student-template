9d413c56e8034b5df25bf7ea3c771be895970439
TyroneMartin <tyrone306@byui.edu>
2026-03-29 05:31:31 -0600

    while loop added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -18,69 +18,77 @@ class Program
         Console.WriteLine("Determines the letter grade");
         Console.WriteLine("##########################################\n");
 
-        Console.Write("What is grade percentage? ");
-        string userGradeInput = Console.ReadLine();
-        int grade = int.Parse(userGradeInput);
-        string letter = "";
-        string letterSymbol = "";
-
-
-
-        if (grade >= 90)
+        string response = "yes";
+        while (response == "yes")
         {
-            letter = "A";
+            Console.Write("What is grade percentage? ");
+            string userGradeInput = Console.ReadLine();
+            int grade = int.Parse(userGradeInput);
+
+            string letter = "";
+            string letterSymbol = "";
+
+
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+
+            else if (grade >= 70)
+            {
+                letter = "C";
+            }
+
+            else if (grade >= 60)
+            {
+                letter = "D";
+            }
+
+            else
+            {
+                letter = "F";
+            }
+
+
+            // Stretch Challenge     
+            // don't worry about the exceptional cases of A+, F+, or F-.
+
+            if (grade % 10 >= 7)
+            {
+                letterSymbol = "+";
+            }
+            else if (grade % 10 < 3)
+            {
+                letterSymbol = "-";
+            }
+            else
+            {
+                letterSymbol = "";
+            }
+
+            if (grade >= 70)
+            {
+                Console.WriteLine("You pass!");
+            }
+            else
+            {
+                Console.WriteLine("You failed!\n");
+            }
+
+            Console.WriteLine($"Your grade is: {letter}{letterSymbol} \n");
+
+            
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine().ToLower();
         }
-
-        else if (grade >= 80)
-        {
-            letter = "B";
-        }
-
-        else if (grade >= 70)
-        {
-            letter = "C";
-        }
-
-        else if (grade >= 60)
-        {
-            letter = "D";
-        }
-
-        else
-        {
-            letter = "F";
-        }
-
-
-
-        // Stretch Challenge     
-        // don't worry about the exceptional cases of A+, F+, or F-.
-
-        if (grade % 10 >= 7)
-        {
-            letterSymbol = "+";
-        }
-        else if (grade % 10 < 3)
-        {
-            letterSymbol = "-";
-        }
-        else
-        {
-            letterSymbol = "";
-        }
-
-        if (grade >= 70)
-        {
-            Console.WriteLine("You pass!");
-        }
-        else
-        {
-            Console.WriteLine("You failed!\n");
-        }
-
-
-        Console.WriteLine($"Your grade is: {letter}{letterSymbol} ");
-
 
     }
+
 }
+
+
