39d5204d9b290865d87b8090a9f1023044c8ad7c
TyroneMartin <tyrone306@byui.edu>
2026-03-29 18:26:04 -0600

    Prep assignment started

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+
+        Console.Clear();
+        Console.WriteLine("==========================");
+        Console.WriteLine("Guess a number!");
+        Console.WriteLine("==========================\n");
+
+        string continueGame = "yes";
+
+        Console.Write("What is the magic number? ");
+
+        string userInputMagicNum;
+        userInputMagicNum = Console.ReadLine();
+        int randomNumber = int.Parse(userInputMagicNum);
+
+        do
+        {
+            // user entry/guess number
+            Console.Write("What is your guess? ");
+
+
+            // manual test 
+            string userResponse = Console.ReadLine();
+            int userGuess = int.Parse(userResponse);
+            string systemResponse = "";
+
+            if (randomNumber == userGuess)
+            {
+                // systemResponse = "You guessed it! \n";
+                Console.Write("You guessed it! \n");
+
+                Console.Write("Do you want to continue? ");
+                continueGame = Console.ReadLine().ToLower();
+                Console.Write("What is the magic number? ");
+
+            }
+
+            else if (randomNumber > userGuess)
+            {
+                systemResponse = "Higher";
+            }
+            else
+            {
+                systemResponse = "Lower";
+            }
+
+            // print systemResponse to the console 
+            Console.WriteLine($"{systemResponse}");
+
+        } while (continueGame == "yes");
+
     }
+
 }
