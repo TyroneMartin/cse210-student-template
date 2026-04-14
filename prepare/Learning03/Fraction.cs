cc1ce022640ea993cb38deacbce8ef365322cd95
TyroneMartin <tyrone306@byui.edu>
2026-04-13 21:48:48 -0600

    fraction class added with uml layout

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Reflection.Metadata.Ecma335;
+
+public class Fraction
+{
+
+    private int _numerator;
+    private int _denominator;
+
+    // Constructor
+    public Fraction() { }
+
+    // methods
+    public Fraction(int wholeNumber)
+    {
+
+    }
+
+    public Fraction(int top, int bottom)
+    {
+
+    }
+
+    public void GetNumerator()
+    {
+
+    }
+
+    public void SetNumerator(int top)
+    {
+
+    }
+
+    public void GetDenominator()
+    {
+
+    }
+
+    public void SetDenominator(int bottom)
+    {
+
+    }
+
+    public string GetFractionString()
+    {
+        return "";
+    }
+
+
+    public double SetDecimalValue()
+    {
+        return 0.0;
+    }
+
+}
